ceb6c0c487c68d5026976bf722303cba90b6e7b6
Михаил Кутякин <michakoutjakin@gmail.com>
2023-01-21 15:22:33 +0300

    14 GridView для карточек

--- 14_animations/hero_card/lib/main.dart
+++ 14_animations/hero_card/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hero_card/space.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,15 +17,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeView(title: 'Animations'),
+      home: const HomeView(title: 'Animations'),
     );
   }
 }
 
 class HomeView extends StatefulWidget {
-  HomeView({Key key, this.title}) : super(key: key);
-
   final String title;
+  const HomeView({Key? key, required this.title}) : super(key: key);
 
   @override
   _HomeViewState createState() => _HomeViewState();
@@ -37,6 +38,48 @@ class _HomeViewState extends State<HomeView> {
         backgroundColor: Colors.black54,
         title: Text(widget.title),
       ),
+      body: GridView.count(crossAxisCount: 2, children: [
+        for (final space in spaces)
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ), // Image border
+                      child: Image.asset(space.image, fit: BoxFit.cover),
+                    ),
+                    Positioned(
+                      bottom: -16,
+                      right: 16,
+                      child: ColoredBox(
+                        color: Colors.orangeAccent,
+                        child: SizedBox.fromSize(
+                          size: const Size.fromRadius(16),
+                          child: const Icon(Icons.add),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    space.description,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ]),
     );
   }
 }
