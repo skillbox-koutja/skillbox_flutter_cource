1a991e063038d253764eb512c3c3d516de9b3eed
Михаил Кутякин <michakoutjakin@gmail.com>
2023-02-18 12:14:48 +0300

    Upgrade project

--- 13_theme/app_theme/lib/main.dart
+++ 13_theme/app_theme/lib/main.dart
@@ -1,41 +1,58 @@
 import 'package:app_theme/theme.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: themeLight,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class _Chip {
+  final String name;
+  final bool isSelect;
+
+  const _Chip(this.name, {this.isSelect = false});
+
+  _Chip select() {
+    return _Chip(name, isSelect: true);
+  }
+
+  _Chip unselect() {
+    return _Chip(name);
+  }
+}
+
+final List<_Chip> _chips = [
+  const _Chip('News'),
+  const _Chip('Book', isSelect: true),
+  const _Chip('Games', isSelect: true),
+  const _Chip('Messages'),
+  const _Chip('People'),
+];
+
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  TabController _tabController;
+  late TabController _tabController;
   int _currentTabIndex = 0;
   bool _isLoading = false;
-  List<Map<String, Object>> _chips = [
-    {'name': 'News', 'isSelect': false},
-    {'name': 'Book', 'isSelect': true},
-    {'name': 'Games', 'isSelect': true},
-    {'name': 'Messages', 'isSelect': false},
-    {'name': 'People', 'isSelect': false}
-  ];
 
   @override
   void initState() {
@@ -47,12 +64,12 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Theme app'),
+        title: const Text('Theme app'),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.save_alt),
-          )
+            icon: const Icon(Icons.save_alt),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -64,19 +81,19 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           });
         },
         currentIndex: _currentTabIndex,
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            title: Text('Home'),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.mail),
-            title: Text('Messages'),
+            label: 'Messages',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
-            title: Text('Profile'),
-          )
+            label: 'Profile',
+          ),
         ],
       ),
       body: TabBarView(
@@ -87,13 +104,13 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             children: <Widget>[
               Text(
                 'Список товаров',
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 12),
                 child: Row(
                   children: [
-                    Expanded(
+                    const Expanded(
                       child: TextField(
                         decoration: InputDecoration(
                           labelText: 'Поиск товаров',
@@ -101,11 +118,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         ),
                       ),
                     ),
-                    RaisedButton(
-                      child: Text('Заказать товар'),
+                    ElevatedButton(
+                      child: const Text('Заказать товар'),
                       onPressed: () async {
                         setState(() => _isLoading = true);
-                        await Future.delayed(Duration(seconds: 1));
+                        await Future<void>.delayed(const Duration(seconds: 1));
                         setState(() => _isLoading = false);
                       },
                     ),
@@ -113,8 +130,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                 ),
               ),
               if (_isLoading)
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.all(12.0),
                   child: CircularProgressIndicator(),
                 ),
               Expanded(
@@ -127,7 +144,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         children: [
                           Expanded(
                             flex: 2,
-                            child: Container(
+                            child: SizedBox(
                               width: double.infinity,
                               child: Image.asset(
                                 'assets/images/img_${index + 1}.jpg',
@@ -135,9 +152,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                               ),
                             ),
                           ),
-                          Expanded(
+                          const Expanded(
                             child: Padding(
-                              padding: const EdgeInsets.all(8.0),
+                              padding: EdgeInsets.all(8.0),
                               child: Text('Краткое описание карточки'),
                             ),
                           ),
@@ -149,68 +166,84 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
               ),
             ],
           ),
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Wrap(
-                spacing: 12,
-                children: [
-                  ..._chips
-                      .map(
-                        (item) => FilterChip(
-                          onSelected: (v) {
-                            setState(() {
-                              item['isSelect'] = v;
-                            });
-                          },
-                          selected: item['isSelect'],
-                          label: Text(item['name']),
-                        ),
-                      )
-                      .toList(),
-                  FilterChip(
-                    onSelected: null,
-                    label: Text('City'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
+          const _Chips(),
+          Padding(
+            padding: const EdgeInsets.all(20),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 InkWell(
                   onTap: () {
-                    showDialog(
+                    showDialog<void>(
                       context: context,
-                      child: AlertDialog(
-                        actions: [
-                          RaisedButton(
-                            child: Text('Да'),
-                            onPressed: () async {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          RaisedButton(
-                            child: Text('Нет'),
-                            onPressed: () async {
-                              Navigator.of(context).pop();
-                            },
-                          )
-                        ],
-                        title: Text('Сменить аватар'),
-                      ),
+                      builder: (context) {
+                        return AlertDialog(
+                          actions: [
+                            ElevatedButton(
+                              child: const Text('Да'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            ElevatedButton(
+                              child: const Text('Нет'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                          title: const Text('Сменить аватар'),
+                        );
+                      },
                     );
                   },
-                  child: CircleAvatar(
+                  child: const CircleAvatar(
                     radius: 40,
                     child: Text('AB'),
                   ),
                 ),
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Chips extends StatefulWidget {
+  const _Chips({Key? key}) : super(key: key);
+
+  @override
+  State<_Chips> createState() => _ChipsState();
+}
+
+class _ChipsState extends State<_Chips> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Wrap(
+        spacing: 12,
+        children: [
+          ..._chips
+              .map(
+                (item) => FilterChip(
+                  onSelected: (v) {
+                    final index = _chips.indexOf(item);
+                    final replacementItem = v ? item.select() : item.unselect();
+                    _chips.replaceRange(index, index + 1, [replacementItem]);
+
+                    setState(() {});
+                  },
+                  selected: item.isSelect,
+                  label: Text(item.name),
+                ),
+              )
+              .toList(),
+          const FilterChip(
+            onSelected: null,
+            label: Text('City'),
           ),
         ],
       ),
