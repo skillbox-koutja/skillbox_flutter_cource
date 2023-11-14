bd1cfbeb56bfa0681a4c000eae361e86f5fc956f
skillbox-koutja <47079939+skillbox-koutja@users.noreply.github.com>
2023-11-14 11:02:51 +0300

    Update artists_fetcher.dart

--- 07_Navigation/albums_route/lib/domain/artist/artists_fetcher.dart
+++ 07_Navigation/albums_route/lib/domain/artist/artists_fetcher.dart
@@ -3,7 +3,7 @@ import 'package:albums_route/fetch_file.dart';
 
 class ArtistsFetcher {
   Future<List<Artist>> fetch() async {
-    var response = await fetchJsonFromAssetsFile('artists.json');
+    final response = await fetchJsonFromAssetsFile('artists.json');
 
     return response.map((jsonArtistData) => Artist.fromMap(jsonArtistData)).toList();
   }
